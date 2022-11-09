21e3fd99d5cdaa6e29c3ec2124c14719a92be0e8
Group031 <Group031@infoway.com>
2022-11-09 15:13:08 +0530

    1st versiion

---
+++ as1.js
@@ -0,0 +1,31 @@
+var numOne, numTwo, res, temp;
+// if (numOne && numTwo) {
+// temp = document.getElementById("res");
+// temp.style.display = "block";
+function add() {
+    numOne = parseInt(document.getElementById("one").value);
+    numTwo = parseInt(document.getElementById("two").value);
+    res = numOne + numTwo;
+    console.log(res)
+    document.getElementById("add").value = res;
+}
+function sub() {
+    numOne = parseInt(document.getElementById("one").value);
+    numTwo = parseInt(document.getElementById("two").value);
+    res = numOne - numTwo;
+    document.getElementById("subtract").value = res;
+}
+function mul() {
+    numOne = parseInt(document.getElementById("one").value);
+    numTwo = parseInt(document.getElementById("two").value);
+    res = numOne * numTwo;
+    document.getElementById("multiply").value = res;
+}
+
+function div() {
+    numOne = parseInt(document.getElementById("one").value);
+    numTwo = parseInt(document.getElementById("two").value);
+    res = numOne / numTwo;
+    document.getElementById("divide").value = res;
+}
+// }
